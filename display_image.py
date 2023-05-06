19066c5ceb935ee604cffb5899b7e3c1870c16eb
timothyliu.tl <18tfl1@queensu.ca>
2023-05-06 01:40:53 +0000

    added first test file to send image

--- display_image.py
+++ display_image.py
@@ -4,7 +4,7 @@ import sys
 from rgbmatrix import RGBMatrix, RGBMatrixOptions
 from PIL import Image
 
-file_path = '../images/seraphine.png'
+file_path = './images/seraphine.png'
 
 image = Image.open(file_path)
 
@@ -13,6 +13,20 @@ options.rows = 32
 options.cols = 64
 options.chain_length = 1
 options.parallel = 1
+options.brightness = 100
+options.pwm_lsb_nanoseconds = 80
 options.hardware_mapping = 'adafruit-hat'
+options.limit_refresh_rate_hz = 150
+options.drop_privileges = False
 
 matrix = RGBMatrix(options = options)
+
+image.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
+
+matrix.SetImage(image.convert('RGB'))
+try:
+    print("press ctrl-c to stop")
+    while True:
+        time.sleep(100)
+except KeyboardInterrupt:
+    sys.exit(0)
