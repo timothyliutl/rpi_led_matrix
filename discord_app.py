5d3d1e327c7e8fc586dcac0114540582a99cd3c2
Timothy <18tfl1@queensu.ca>
2023-05-13 05:41:39 +0100

    converted to class

--- discord_app.py
+++ discord_app.py
@@ -1,4 +1,6 @@
+from typing import Any
 import discord
+from discord.flags import Intents
 from dotenv import load_dotenv
 import os
 from discord.ext import tasks
@@ -16,38 +18,41 @@ intents.members = True
 client = discord.Client(intents=intents)
 token = os.getenv('DISCORD_TOKEN')
 user_id = os.getenv('DISCORD_ID')
-led_matrix = MatrixClass()
-lock = threading.Lock()
+server_id = os.getenv('DISCORD_SERVER_ID')
 
-def update_display():
-    while True:
-        with lock:
-            led_matrix.update_display()
-            time.sleep(0.2)
+#adding class based structure for discord client
+class DiscordClient(discord.Client):
+    def __init__(self, *, intents: Intents, **options: Any) -> None:
+        super().__init__(intents=intents, **options)
+        self.led_matrix = MatrixClass()
+        self.lock = threading.Lock()
+        self.t1 = threading.Thread(target=self.update_display)
+        self.t1.setDaemon(True)
+        self.t1.start()
 
+    def update_display(self):
+        while True:
+            with self.lock:
+                self.led_matrix.update_display()
+                time.sleep(0.2)
 
-t1 = threading.Thread(target=update_display)
-t1.setDaemon(True)
-t1.start()
+    async def on_ready(self):
+        print("Bot Ready")
 
-@client.event
-async def on_ready():
-    print("Bot Ready")
-    # input function to check for current status
-
-@client.event
-async def on_presence_update(before, after):
-    if after.activities:
-        activities = after.activities
-        for act in activities:
-            if isinstance(act, discord.Spotify) and after.id ==int(user_id):
-                song_name = act.title
-                song_artist =act.artists[0]
-                print(song_name, song_artist)
-                album_cover_url = act.album_cover_url
-                lock.acquire()
-                led_matrix.update_song(album_cover_url, str(song_name), str(song_artist))
-                lock.release()
-client.run(token)
+    async def on_presence_update(self, before, after):
+        if after.activities:
+            activities = after.activities
+            for act in activities:
+                if isinstance(act, discord.Spotify) and after.id ==int(user_id):
+                    song_name = act.title
+                    song_artist =act.artists[0]
+                    print(song_name, song_artist)
+                    album_cover_url = act.album_cover_url
+                    self.lock.acquire()
+                    self.led_matrix.update_song(album_cover_url, str(song_name), str(song_artist))
+                    self.lock.release()
+    
+class_client = DiscordClient(intents=intents)
+class_client.run(token)
 
 
